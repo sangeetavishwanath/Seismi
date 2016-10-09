3e599169766f544483bf3979d9fcad8d975fbe0b
Sangeeta Vishwanath <sangeeta.vishwanath@gmail.com>
2016-10-09 15:51:21 +1100

    Make the presenter fetch earthquakes when the earthquakes view is loaded.

--- Seismi/EarthquakesSummary/Presenter/EarthquakesPresenter.m
+++ Seismi/EarthquakesSummary/Presenter/EarthquakesPresenter.m
@@ -8,11 +8,43 @@
 
 #import "EarthquakesPresenter.h"
 
+#import <SeismiKit/SeismiKit.h>
+
+@interface EarthquakesPresenter () <EarthquakesControllerDelegate>
+
+@property (nonatomic, strong) EarthquakesController *earthquakesController;
+
+@end
+
 @implementation EarthquakesPresenter
 
 - (void)earthquakesViewDidLoad
 {
     [self.viewSurface displayLoadingIndicator];
+
+    self.earthquakesController = [[EarthquakesController alloc] init];
+    self.earthquakesController.delegate = self;
+
+    [self loadEarthquakes];
+}
+
+#pragma mark - Helpers
+
+- (void)loadEarthquakes
+{
+    [self.earthquakesController retrieveEarthquakes];
+}
+
+#pragma mark - EarthquakesControllerDelegate methods
+
+- (void)earthquakesController:(EarthquakesController *)controller didRetrieveEarthquakes:(NSArray<Earthquake *> *)earthquakes
+{
+
+}
+
+- (void)earthquakesController:(EarthquakesController *)controller didFailToRetrieveEarthquakesWithError:(NSError *)error
+{
+    
 }
 
 @end
